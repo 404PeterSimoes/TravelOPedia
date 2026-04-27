4c976e31f50340b2f5ad20aa49669750ca98cb15
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-27 12:51:02 +0100

    Section 11 - 173 - Basic Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-grow-1 row">
+        <div className="col-12">hello</div>
+      </main>
+      <Footer />
+    </div>
   </StrictMode>,
-)
+);
