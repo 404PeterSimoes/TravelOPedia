77faaf4b6104055b108804145f01c9340652feda
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-28 16:51:12 +0100

    Section 11 - 188 - Separating out the Destination code from the rest

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,13 +1,9 @@
-import {
-  useDeleteDestinationMutation,
-  useGetAllDestinationsQuery,
-} from '../api/destinationApi';
+import { useGetAllDestinationsQuery } from '../api/destinationApi';
+import Destination from './Destination';
 
 function DestinationList() {
   const { data, isLoading, isSuccess, isError, error } =
     useGetAllDestinationsQuery();
-
-  const [deleteDestination] = useDeleteDestinationMutation();
 
   let content;
 
@@ -15,20 +11,7 @@ function DestinationList() {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
     content = data.map((destination) => (
-      <div className="row py-1 border-top" key={destination.id}>
-        <div className="col-5 offset-1">
-          {destination.city}, {destination.country}
-        </div>
-        <div className="col-2 text-info">{destination.daysNeeded} days</div>
-        <div className="col-3">
-          <button
-            className="btn form-control btn-danger"
-            onClick={() => deleteDestination({ id: destination.id })}
-          >
-            Delete
-          </button>
-        </div>
-      </div>
+      <Destination destination={destination} key={destination.id} />
     ));
   } else if (isError) {
     content = <p>{error}</p>;
