dbb4761ed8045f7f31866944307e6e898e0edb1e
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-28 12:56:51 +0100

    Section 11 - 183 - Tags

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -3,19 +3,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const destinationApi = createApi({
   reducerPath: 'apidestination',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/' }),
+  tagTypes: ['Destinations'],
   endpoints: (builder) => ({
     // QUERY -> GET
     // MUTATION -> POST, PUT, DELETE
 
     getAllDestinations: builder.query({
       query: () => 'destination',
+      providesTags: ['Destinations'],
     }),
+    // getAllDestinationsById: builder.query({
+    //   query: (id) => `destination/${id}`,
+    //   providesTags: (result, error, id) => [{ type: 'Destination', id }],
+    // }),
     addDestination: builder.mutation({
       query: (destinationObj) => ({
         url: 'destination',
         method: 'POST',
         body: destinationObj,
       }),
+      invalidatesTags: ['Destinations'],
     }),
     updateDestination: builder.mutation({
       query: (destinationObj) => ({
@@ -23,13 +30,16 @@ export const destinationApi = createApi({
         method: 'PUT',
         body: destinationObj,
       }),
+      invalidatesTags: ['Destinations'],
     }),
     deleteDestination: builder.mutation({
       query: (id) => ({
         url: `destination/${id}`,
         method: 'DELETE',
       }),
+      // invalidatesTags: (result, error, id) => [{ type: 'Destination', id }],
     }),
+    invalidatesTags: ['Destinations'],
   }),
 });
 
