7b436766a4c785734e9814603c9f6b50fb76af2c
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-28 13:51:51 +0100

    Section 11 - 184 - Destination List UI

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -10,12 +10,15 @@ function DestinationList() {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
     content = data.map((destination) => (
-      <article key={destination.id}>
-        <div className="text-center text-info p-2">
-          {destination.city}, {destination.country} - {destination.daysNeeded}{' '}
-          days
+      <div className="row py-1 border-top" key={destination.id}>
+        <div className="col-5 offset-1">
+          {destination.city}, {destination.country}
         </div>
-      </article>
+        <div className="col-2 text-info">{destination.daysNeeded} days</div>
+        <div className="col-3">
+          <button className="btn form-control btn-danger">Delete</button>
+        </div>
+      </div>
     ));
   } else if (isError) {
     content = <p>{error}</p>;
