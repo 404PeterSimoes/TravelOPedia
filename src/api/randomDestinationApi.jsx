3e3372c51b17c03e416081cba433d6f89cfd7498
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-05 11:26:31 +0100

    Section 11 - 193 - Add random destination API

---
+++ src/api/randomDestinationApi.jsx
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const randomDestinationApi = createApi({
+  reducerPath: 'apiRandomDestination',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://randomuser.me/api/' }),
+  tagTypes: ['Destinations'],
+  endpoints: (builder) => ({
+    // QUERY -> GET
+    // MUTATION -> POST, PUT, DELETE
+
+    getRandomDestination: builder.query({
+      query: () => ({
+        method: 'GET',
+      }),
+      transformResponse: (res) => {
+        // Extract only city and country from the random user data
+
+        const user = res.results[0];
+        return {
+          city: user.location.city,
+          country: user.location.country,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetRandomDestinationQuery } = randomDestinationApi;
